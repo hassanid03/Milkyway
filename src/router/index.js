77bbed078a4ef83bad725e2563a530b69956e874
hochstegerL03 <72703264+hochstegerL03@users.noreply.github.com>
2022-12-21 17:09:10 +0100

    U1-L

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue';
 import Goals from '../views/GoalsView.vue';
 import Careers from '../views/CareersView.vue';
 import Trips from '../views/TripsView.vue';
+import LegalNotice from '../views/LegalNotice.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/trips',
       name: 'Trips',
       component: Trips,
+    },
+    {
+      path: '/legalnotice',
+      name: 'Legal Notice',
+      component: LegalNotice,
     },
   ],
 });
