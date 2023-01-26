cb6d91c23558e823b96102d5ce8d9b9e4b321209
Raphael Losko <losko.r04@htlwienwest.at>
2023-01-26 10:21:10 +0100

    pinia store planets

---
+++ src/stores/planets.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const usePlanetStore = defineStore('planets', {
+  state: () => ({
+    mercury: [],
+    earth: [],
+  }),
+  actions: {
+    async getMercury() {
+      const { data } = await axios.get('http://localhost:3000/milkyway/planets/1');
+      this.mercury = data;
+    },
+    async getEarth() {
+      const { data } = await axios.get('http://localhost:3000/milkyway/planets/2');
+      this.earth = data;
+    },
+  },
+  getters: {},
+});
